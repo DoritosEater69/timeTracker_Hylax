c6d09ff9e4c55d79a37792ed33baf024c2cb459d
pawel-hylax <85232637+pawel-hylax@users.noreply.github.com>
2022-09-30 10:36:44 +0200

    php scripts written

--- tracker.php
+++ tracker.php
@@ -1,20 +1,35 @@
 <?php
-
+require_once "../../wp-load.php";
 global $wpdb;
+$name = $_POST['name'];
+$projekt = $_POST['projekt'];
 
-isset($_POST["aktiv"]){
+if (isset($_POST["aktiv"])) {
+	$results = $wpdb->get_results("SELECT *  FROM `timetracker` WHERE `name` = '" . $name . "' AND `aktiv` = 'true'");
+	if ($results) {
+		$results = $wpdb->get_results("UPDATE `timetracker` SET `aktiv` = 'false' , `ende` = now() WHERE `name` = '" . $name . "' AND `aktiv` = 'true'");
+	}
 
+	$results = $wpdb->get_results("INSERT INTO timetracker (name,projekt,aktiv) VALUES ('" . $name . "', '" . $projekt . "', 'true')");
+	$results = $wpdb->get_results("SELECT *  FROM `timetracker` WHERE `name` = '" . $name . "' AND `aktiv` = 'true'");
+	echo json_encode($results);
 }
 
-isset($_POST["pause"]){
-    
+if (isset($_POST["pause"])) {
+
+	$results = $wpdb->get_results("UPDATE `timetracker` SET `aktiv` = 'false' , `ende` = now() WHERE `name` = '" . $name . "' AND `aktiv` = 'true'");
+	$results = $wpdb->get_results("INSERT INTO timetracker (name,projekt,aktiv,pause) VALUES ('" . $name . "', 'Pause', 'true','" . $projekt . "')");
+	$results = $wpdb->get_results("SELECT *  FROM `timetracker` WHERE `name` = '" . $name . "' AND `aktiv` = 'true'");
+	echo json_encode($results);
 }
 
-isset($_POST["feierabend"]){
-    
+if (isset($_POST["feierabend"])) {
+	$results = $wpdb->get_results("UPDATE `timetracker` SET `aktiv` = 'false' , `ende` = now() WHERE `name` = '" . $name . "' AND `aktiv` = 'true'");
+	$results = $wpdb->get_results("SELECT *  FROM `timetracker` WHERE `aktiv` = 'true'");
+	echo json_encode($results);
 }
 
-isset($_POST["init"]){
-   	$results = $wpdb->get_results("SELECT *  FROM ".$wpdb->prefix."tracker WHERE aktiv IS 'true' OR pause IS 'true'");
+if (isset($_POST["init"])) {
+	$results = $wpdb->get_results("SELECT *  FROM `timetracker` WHERE `aktiv` = 'true'");
+	echo json_encode($results);
 }
-?>
